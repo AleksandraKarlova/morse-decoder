050866f6a36595d0dea589232c45b12390c6de1a
Aleksandra Karlova <aleksandra.karlova10@gmail.com>
2024-09-01 19:44:17 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -35,10 +35,37 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********': ' ',
 };
 
 function decode(expr) {
     // write your solution here
+    let final = '';
+
+    for (let i = 0; i < expr.length / 10; i++) {
+        const slice = expr.slice(10 * i, 10 * i + 10);
+
+        if (slice.includes('*')) {
+            final = final + ' ';
+            continue;
+        }
+
+        const word = slice.slice(slice.indexOf('1'));
+
+        let morse = '';
+
+        for (let j = 0; j < word.length; j = j + 2) {
+            if (word[j + 1] === '0') {
+                morse = morse + '.';
+            } else {
+                morse = morse + '-';
+            }
+        }
+
+        final = final + MORSE_TABLE[morse];
+    }
+
+    return final;
 }
 
 module.exports = {
